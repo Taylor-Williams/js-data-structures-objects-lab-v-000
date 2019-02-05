eff685ed4837417ce134361efbc26981862e6bc3
Taylor Williams <taylor@thewilliams.net>
2019-02-05 01:21:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function updateDriverWithKeyAndValue(driver, key, value){
   return Object.assign({}, driver, {[key]:value})
 }
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
-  return driver.key = vlue
+  return driver.key = value
 }
